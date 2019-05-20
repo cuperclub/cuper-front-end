c93b10720a4819bad7f2bc88aa458ab55014fa3f
Darwin Gualotuña <daosgc@gmail.com>
2019-05-20 14:31:23 -0500

    create office model and add principal interface for it

--- src/app/components/card-office/card-office.component.ts
+++ src/app/components/card-office/card-office.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Office } from '../../models';
 
 @Component({
   selector: 'app-card-office',
@@ -6,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./card-office.component.scss']
 })
 export class CardOfficeComponent implements OnInit {
-  @Input() offices: object [];
+  @Input() offices: Office [];
 
   constructor() { }
 
---
+++ src/app/models/office/office.model.ts
@@ -0,0 +1,10 @@
+export interface Office {
+  id?: number;
+  created_at?: Date;
+  updated_at?: Date;
+  name?: string;
+  email?: string;
+  address?: string;
+  active?: boolean;
+  company_id?: number;
+}
