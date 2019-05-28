6b11ba12af5afcb428cb9091ac3069958dc401d8
Darwin Gualotuña <daosgc@gmail.com>
2019-05-28 11:45:21 -0500

    Add card counter

---
+++ src/app/components/card-counter/card-counter.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cuper-card-counter',
+  templateUrl: './card-counter.component.html',
+  styleUrls: ['./card-counter.component.scss']
+})
+export class CardCounterComponent implements OnInit {
+  @Input() title: string;
+  @Input() count: number;
+  @Input() description: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Disponible';
+    this.count = 10;
+    this.description = 'Personas';
+  }
+
+}
