333023ddebbc08cc963a9548da91af7beceac941
Darwin Gualotuña <daosgc@gmail.com>
2019-06-14 14:35:06 -0500

    add enviroment mapbox

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBase: 'https://cuper-backend.herokuapp.com'
+  apiBase: 'https://cuper-backend.herokuapp.com',
+  mapboxAuth: 'pk.eyJ1IjoiZGFvc2djIiwiYSI6ImNqa2VnbW9ldTNiYWUzcG1rdmoyZ29jcnkifQ.LhnLZHX-gNhQefUN2iihPg'
 };
