b581922a9ab0487e2f67c634c46c6512fa0915fc
Darwin Gualotuña <daosgc@gmail.com>
2019-06-26 15:40:39 -0500

    show input transactions

--- src/app/pages/employee/transactions/transactions.component.ts
+++ src/app/pages/employee/transactions/transactions.component.ts
@@ -29,33 +29,13 @@ export class TransactionsComponent implements OnInit {
     //mockdata
     this.columns = [
       {
-        label: 'position',
-        displayName: 'Nº'
+        label: 'points',
+        displayName: 'Puntos'
       },
       {
-        label: 'name',
-        displayName: 'Name'
-      },
-      {
-        label: 'weight',
-        displayName: 'Weight'
-      },
-      {
-        label: 'symbol',
-        displayName: 'Simbol'
+        label: 'created_at',
+        displayName: 'Fecha'
       }
-    ];
-    this.exampleData = [
-      {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-      {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-      {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-      {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-      {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-      {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-      {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-      {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-      {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-      {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
     ];
 
     this.rightButton = {
@@ -66,7 +46,7 @@ export class TransactionsComponent implements OnInit {
       action: this.onOutputTransaction,
       label: 'transactions.output_transaction'
     };
-    // this.transactionService.getAll().subscribe(resp => console.log(resp));
+    this.transactionService.getAll().subscribe(resp => this.exampleData = resp['transaction_inputs']);
   }
 
   onListerCustomerSearch(user){
