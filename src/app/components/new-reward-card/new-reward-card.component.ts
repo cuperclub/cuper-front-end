f2ad625ef86f16bb38d3a52f6beaca886af13d62
Darwin Gualotuña <daosgc@gmail.com>
2019-08-30 14:58:19 -0500

    Use form control in new reward form and show the errors message correctly

--- src/app/components/new-reward-card/new-reward-card.component.ts
+++ src/app/components/new-reward-card/new-reward-card.component.ts
@@ -7,6 +7,7 @@ import { MatSnackBar } from '@angular/material';
 import { TranslateService } from '@ngx-translate/core';
 import { OfficeFormComponent } from '../office-form/office-form.component';
 import { MatDialog } from '@angular/material';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface OptionSquare {
   id?: number,
@@ -26,16 +27,21 @@ interface FileOptions {
 })
 export class NewRewardCardComponent implements OnInit {
   currentDate: Date = new Date();
-  reward: Promotion = {
+  defaultPromotion: Promotion = {
+    title: '',
+    total_rewards: 0,
+    points_required: 0,
     unlimited: false,
     start_at: this.currentDate,
-    end_at: this.currentDate
+    end_at: this.currentDate,
+    office: {}
   };
   myCompany: Company;
   myOffices: OptionSquare[] = [];
   isEditRoute: boolean = false;
   officeSelected: OptionSquare;
   uploadFile: FileOptions;
+  rewardFormGroup: FormGroup;
 
   constructor(
     private router: Router,
@@ -45,7 +51,8 @@ export class NewRewardCardComponent implements OnInit {
     private promotionService: PromotionService,
     private message: MatSnackBar,
     private translate: TranslateService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private _formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
@@ -60,11 +67,33 @@ export class NewRewardCardComponent implements OnInit {
       });
     });
     const rewardId = parseInt(this.route.snapshot.paramMap.get('rewardId'));
+    this.rewardFormGroup = this._formBuilder.group({
+      title: [this.defaultPromotion.title, [Validators.required]],
+      terms: [this.defaultPromotion.terms, []],
+      total_rewards: [this.defaultPromotion.total_rewards, [Validators.required]],
+      points_required: [this.defaultPromotion.points_required, [Validators.required]],
+      unlimited: [this.defaultPromotion.unlimited, []],
+      start_at: [this.defaultPromotion.start_at, [Validators.required]],
+      end_at: [this.defaultPromotion.end_at, [Validators.required]],
+      office_id: [this.defaultPromotion.office.id, [Validators.required]],
+    });
     if(rewardId){
       this.isEditRoute = true;
       this.promotionService.getPromotion(rewardId).subscribe(reward => {
-        this.reward = reward;
         this.officeSelected = this.myOffices.find(office => office.id === reward.office.id);
+        reward.start_at = new Date(reward.start_at);
+        reward.end_at = new Date(reward.end_at);
+        const currentReward = Object.assign(this.defaultPromotion, reward);
+        this.rewardFormGroup = this._formBuilder.group({
+          title: [currentReward.title, [Validators.required]],
+          terms: [currentReward.terms, []],
+          total_rewards: [currentReward.total_rewards, [Validators.required]],
+          points_required: [currentReward.points_required, [Validators.required]],
+          unlimited: [currentReward.unlimited, []],
+          start_at: [currentReward.start_at, []],
+          end_at: [currentReward.end_at, []],
+          office_id: [currentReward.office.id, []],
+        });
       });
     }else {
       this.officeSelected = {};
@@ -72,15 +101,23 @@ export class NewRewardCardComponent implements OnInit {
   }
 
   onSubmitReward() {
-    if(this.isEditRoute){
-      this.editReward();
+    if(this.officeSelected.hasOwnProperty('id')){
+      if(this.isEditRoute){
+        this.editReward();
+      }else {
+        this.saveReward();
+      }
     }else {
-      this.saveReward();
+      this.translate.get('reward.office_required').subscribe((message: string) => {
+        this.message.open(message, '', {
+          duration: 2000
+        });
+      });
     }
   }
 
   saveReward() {
-    const inputPromotion = this.getPromotionFormData(this.reward);
+    const inputPromotion = this.getPromotionFormData(this.rewardFormGroup.value);
     this.promotionService.createPromotion(inputPromotion, this.officeSelected.id)
     .subscribe(
       () =>    this.onSuccess('common.messages.created'),
@@ -89,8 +126,9 @@ export class NewRewardCardComponent implements OnInit {
   }
 
   editReward() {
-    const inputPromotion = this.getPromotionFormData(this.reward);
-    this.promotionService.updatePromotion(inputPromotion, this.reward.id, this.officeSelected.id)
+    const inputPromotion = this.getPromotionFormData(this.rewardFormGroup.value);
+    const rewardId = parseInt(this.route.snapshot.paramMap.get('rewardId'));
+    this.promotionService.updatePromotion(inputPromotion, rewardId, this.officeSelected.id)
     .subscribe(
       () =>    this.onSuccess('common.messages.updated'),
       error =>  this.onError(error)
@@ -109,9 +147,9 @@ export class NewRewardCardComponent implements OnInit {
   }
 
   onError(resp): void {
-    this.message.open(resp.errors, '', {
-      duration: 2000
-    });
+    for (let key in resp.error) {
+      this.rewardFormGroup.controls[key].setErrors({'backendError': resp.error[key]});
+    }
   }
 
   onNewOffice = () => {
@@ -149,11 +187,17 @@ export class NewRewardCardComponent implements OnInit {
   }
 
   onChangeTotalRewards = (resp) => {
-    this.reward.total_rewards = resp.checked ? undefined : this.reward.total_rewards;
+    const totalRewardsAction = resp.checked ? 'disable' : 'enable';
+    this.rewardFormGroup.controls.total_rewards[totalRewardsAction]();
+    const totalRewardsValue = resp.checked ? undefined : this.rewardFormGroup.controls.total_rewards.value;
+    this.rewardFormGroup.controls.total_rewards.setValue(totalRewardsValue);
   }
 
   onChangeStartTime = (startTime) => {
-    this.reward.end_at = (this.reward.end_at.valueOf() < startTime.value.valueOf()) ? startTime.value : this.reward.end_at;
+    if(startTime.value){
+      const endAtValue = (this.rewardFormGroup.value.end_at.valueOf() < startTime.value.valueOf()) ? startTime.value : this.rewardFormGroup.value.end_at;
+      this.rewardFormGroup.controls.end_at.setValue(endAtValue);
+    }
   }
 
 }
