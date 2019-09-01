96a277089ddbe20c0d9d90b44b795e82e1f1fd60
Lenin Capa <leviskp87@gmail.com>
2019-08-31 22:15:28 -0500

    remove export

--- src/app/components/table/partials/index.ts
+++ src/app/components/table/partials/index.ts
@@ -3,4 +3,3 @@ export * from './user-cell/user-cell.component';
 export * from './promotion-cell/promotion-cell.component';
 export * from './roles-cell/roles-cell.component';
 export * from './actions-cell/actions-cell.component';
-export * from './transaction-type-cell/transaction-type-cell.component';
