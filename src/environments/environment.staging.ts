dde85080da6d5c7faaf9f4c466762b31ee7f4ea5
Darwin Gualotuña <daosgc@gmail.com>
2019-09-06 10:30:50 -0500

    Add variables to pusher in staging environment

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   apiBase: 'https://cuper-backend.herokuapp.com',
-  mapboxAuth: 'pk.eyJ1IjoiZGFvc2djIiwiYSI6ImNqa2VnbW9ldTNiYWUzcG1rdmoyZ29jcnkifQ.LhnLZHX-gNhQefUN2iihPg'
+  mapboxAuth: 'pk.eyJ1IjoiZGFvc2djIiwiYSI6ImNqa2VnbW9ldTNiYWUzcG1rdmoyZ29jcnkifQ.LhnLZHX-gNhQefUN2iihPg',
+  pusher_key: '85e46b162288859d70a0',
+  pusher_cluster: 'us3'
 };
