b490a60d5d042b2d74c805fc80cd88b02c8b72fb
Darwin Gualotuña <daosgc@gmail.com>
2019-09-06 11:17:33 -0500

    update pusher keys

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiBase: 'https://cuper-backend.herokuapp.com',
   mapboxAuth: 'pk.eyJ1IjoiZGFvc2djIiwiYSI6ImNqa2VnbW9ldTNiYWUzcG1rdmoyZ29jcnkifQ.LhnLZHX-gNhQefUN2iihPg',
-  pusher_key: '85e46b162288859d70a0',
+  pusher_key: 'b5429473c9fd39c946b1',
   pusher_cluster: 'us3'
 };
