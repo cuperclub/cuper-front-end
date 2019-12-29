458ed96d8b2364eb244ae4c7721463a6bc1d3f15
Darwin Gualotuña <daosgc@gmail.com>
2019-12-29 10:10:25 -0500

    Add environment staging variable to social networks

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiBase: 'https://cuper-backend.herokuapp.com',
   mapboxAuth: 'pk.eyJ1IjoiZGFvc2djIiwiYSI6ImNqa2VnbW9ldTNiYWUzcG1rdmoyZ29jcnkifQ.LhnLZHX-gNhQefUN2iihPg',
   pusher_key: 'b5429473c9fd39c946b1',
-  pusher_cluster: 'us3'
+  pusher_cluster: 'us3',
+  success_state: 'http://cuper-backend.herokuapp.com/home/dashboard'
 };
