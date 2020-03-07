c22d4787e2a8afde4d02811cddafc71a02d30c7f
Lenin Capa <lenin.capa@hpe.com>
2020-03-07 10:14:37 -0500

    card company plan

---
+++ src/app/components/card-company-plan/card-company-plan.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { CardCompanyPlanComponent } from './card-company-plan.component';
+
+// describe('CardCompanyPlanComponent', () => {
+//   let component: CardCompanyPlanComponent;
+//   let fixture: ComponentFixture<CardCompanyPlanComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ CardCompanyPlanComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(CardCompanyPlanComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ src/app/components/card-company-plan/card-company-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cuper-card-company-plan',
+  templateUrl: './card-company-plan.component.html',
+  styleUrls: ['./card-company-plan.component.scss']
+})
+export class CardCompanyPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
